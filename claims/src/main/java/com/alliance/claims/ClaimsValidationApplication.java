6141062e37d6955cc5e4243c12b988f6d740b095
JunkyDubs <73289735+barbecue2304@users.noreply.github.com>
2022-04-02 15:55:03 +0700

    Claims

---
+++ claims/src/main/java/com/alliance/claims/ClaimsValidationApplication.java
@@ -0,0 +1,12 @@
+package com.alliance.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsValidationApplication.class, args);
+	}
+}
---
+++ claims/src/test/java/com/alliance/claims/ClaimsValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alliance.claims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
